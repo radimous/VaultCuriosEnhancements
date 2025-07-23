d7287a63391955fbe5fe628fe09b62e35a7c7b1d
radimous <jurda92@gmail.com>
2025-07-23 18:51:11 +0200

    support curios in the antique predicate

---
+++ src/main/java/com/radimous/vaultcuriosenhancements/mixin/AntiqueConditionPlayerHasItemMixin.java
@@ -0,0 +1,24 @@
+package com.radimous.vaultcuriosenhancements.mixin;
+
+import iskallia.vault.antique.condition.AntiqueConditionPlayerHasItem;
+import iskallia.vault.core.world.data.item.ItemPredicate;
+import net.minecraft.server.level.ServerPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.theillusivec4.curios.api.CuriosApi;
+
+@Mixin(value = AntiqueConditionPlayerHasItem.class, remap = false)
+public class AntiqueConditionPlayerHasItemMixin {
+    @Inject(method = "hasItem", at = @At(value = "HEAD"), cancellable = true)
+    private void hasItem(final ServerPlayer player, ItemPredicate pred, CallbackInfoReturnable<Boolean> cir) {
+        var curios = CuriosApi.getCuriosHelper().findCurios(player);
+        for (var curio : curios) {
+            if (pred.test(curio.stack())){
+                cir.setReturnValue(true);
+                return;
+            }
+        }
+    }
+}
